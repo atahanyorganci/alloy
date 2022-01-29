eddff01c14aefe58492cff2af52ba6b6b6d3d1bc
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-01-29 23:26:46 +0300

    Implement `Into<&'a str>` for `Input<'a>`

--- src/parser/input.rs
+++ src/parser/input.rs
@@ -86,3 +86,9 @@ impl<'a> Compare<&str> for Input<'a> {
 }
 
 impl<'a> UnspecializedInput for Input<'a> {}
+
+impl<'a> Into<&'a str> for Input<'a> {
+    fn into(self) -> &'a str {
+        self.input
+    }
+}
