4432b3ab210d6b7e00215fbadba3d73dabb1b289
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-02-04 12:10:13 +0300

    Implement `fmt::Display` for `Spanned<T>` where `T: fmt::Display`
    Display formatting of `Spanned<T>` is delegated to the `T` type.

--- src/parser/spanned.rs
+++ src/parser/spanned.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /// `Spanned<T>` is a wrapper around `T` that holds start and
 /// end positions of the AST node in the source code.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -13,5 +15,14 @@ where
 {
     fn eq(&self, other: &T) -> bool {
         self.ast == *other
+    }
+}
+
+impl<T> fmt::Display for Spanned<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.ast)
     }
 }
